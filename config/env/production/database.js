1cf081a0e40015a37bed8b0759d81239252de1ce
antoine.tech <antoinehadrienleguillou@outlook.com>
2020-11-08 09:12:01 +0100

    reviewing configs

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'postgres',
         host: config.host,
-        port: config.port,
+        port: process.env.PORT,
         database: config.database,
         username: config.user,
         password: config.password,
